4fa15d962ffc3e2870a6d21e17161f3bfd4dfc9a
yongcrane96 <dydgkr3363@naver.com>
2025-06-19 18:45:23 +0900

    fix-2차원 배열로 인플레이스 알고리즘

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Main {
         public static int[][] solution(int[][] matrix, int r) {
-            int n = matrix.length;
             int rotations = r % 4;
 
             for (int i = 0; i < rotations; i++) {
@@ -14,15 +13,23 @@ class Main {
 
         private static void rotateOnce(int[][] matrix) {
             int n = matrix.length;
-            for (int i = 0; i < n / 2; i++) {
-                for (int j = i; j < n - 1 - i; j++) {
+
+            for (int i = 0; i < n/2; i++) {
+                for (int j = 0; j < n - 1 - i ; j++) {
                     int temp = matrix[i][j];
-                    matrix[i][j] = matrix[n - 1 - j][i];
-                    matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
-                    matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
-                    matrix[j][n - 1 - i] = temp;
+                    // 1 2    3 1
+                    // 3 4    4 2
+                    matrix[i][j] = matrix[n-1-j][i];
+                    matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                    matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                    matrix[j][n-1-i] = temp;
+
                 }
             }
+                        // 1 2    3 1
+                        // 3 4    4 2
+
+
         }
 
     public static void main(String[] args) {
@@ -31,3 +38,7 @@ class Main {
         System.out.println(Arrays.deepToString(solution(matrix,r)));
     }
 }
+
+// 1 2 3  7 4 1
+// 4 5 6  8 5 2
+// 7 8 9  9 6 3
