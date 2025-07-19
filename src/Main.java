f6b2490366ac2581b769f59d0b1afc26de94ce94
yongcrane96 <dydgkr3363@naver.com>
2025-07-19 20:13:10 +0900

    이진트리 문제
    이진트리 문제

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,50 @@
 class Main {
+    private char[] tree; // 이진트리 구조로 메시지 저장할 배열
+    private StringBuilder resultBuilder; // 후위 순위 결과를 달을 문자열 빌더
 
-    public static int[] solution(int[] sales, int[] fromMonth, int[] toMonth){
-        int n = sales.length;
-        long[] prefixSum = new long[n+1]; // 누적합 배열(0부터 시작)
+    public String solution(String message) {
+        int n = message.length();
+        tree = new char[n];
 
         for (int i = 0; i < n; i++) {
-            prefixSum[i+1] = prefixSum[i] + sales[i]; // 누적합 계산
-        }
-        // [0, 100, 201, 303, 406, 510]
-
-        int queryCount = fromMonth.length;
-        int[] answer = new int[queryCount];
-
-        for (int i = 0; i < queryCount; i++) {
-            int startIndex = fromMonth[i]; //1,2
-            int endIndex = toMonth[i]; //2, 4
-            // 309
-            long sum = prefixSum[endIndex+1] - prefixSum[startIndex]; //구간합 계산
-            answer[i] = (int) sum;
+            tree[i] = message.charAt(i);
         }
 
-        return answer;
+        // 결과 문자열을 쌓아갈 StringBuilder 생성
+        resultBuilder = new StringBuilder();
+
+        // 후위 순회 시작 (루트 노드는 인덱스 0)
+        postOrdertraversal(0);
+
+        return resultBuilder.toString();
     }
 
+    private void postOrdertraversal(int index) {
+        if (index >= tree.length) return;
 
 
+        int leftChildIndex = 2 * index + 1;
+        int rightChildIndex = 2 * index + 2;
+
+        postOrdertraversal(leftChildIndex);
+        postOrdertraversal(rightChildIndex);
+        resultBuilder.append(tree[index]);
+
+    }
+
+    private void preOrdertraversal(int index) {
+        if (index >= tree.length) return;
+
+        int leftChildIndex = 2 * index + 1;
+        int rightChildIndex = 2 * index + 2;
+
+        resultBuilder.append(tree[index]);
+        preOrdertraversal(leftChildIndex);
+        preOrdertraversal(rightChildIndex);
+    }
     public static void main(String[] args) {
-        int[] sales = {100,101,102,103,104};
-        int[] fromMonth = {1, 2};
-        int[] toMonth = {2, 4};
-
-        System.out.println(solution(sales, fromMonth, toMonth));
+        String message = "ABCDEF";
+        Main main = new Main();
+        System.out.println(main.solution(message));
     }
 }
